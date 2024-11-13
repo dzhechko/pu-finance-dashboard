5942fc81965c202c2888c89e08d95e2ee50a6ee7
Dmitry Zhechkov <dzhechkov@yandex-team.ru>
2024-11-13 20:46:16 +0300

    railway commit 03

---
+++ dashboards.py
@@ -0,0 +1,22 @@
+import streamlit as st
+from utils.logger import log_info, log_error
+
+def show_dashboard_page():
+    st.title("Панель управления")
+    st.write("Панель управления в разработке...")
+
+def show_net_worth_page():
+    st.title("Чистая стоимость")
+    st.write("Страница чистой стоимости в разработке...")
+
+def show_income_expenses_page():
+    st.title("Доходы и расходы")
+    st.write("Страница доходов и расходов в разработке...")
+
+def show_expense_breakdown_page():
+    st.title("Разбивка расходов")
+    st.write("Страница разбивки расходов в разработке...")
+
+def show_budget_page():
+    st.title("Бюджет")
+    st.write("Страница бюджета в разработке...") 
